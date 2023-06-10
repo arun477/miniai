95e6cf23af69815ec7304a06147a367bc19cebf2
arun <arun@layeriv.com>
2023-06-10 19:40:08 +0530

    datasets notebook recreated

--- miniai/_modidx.py
+++ miniai/_modidx.py
@@ -6,4 +6,12 @@ d = { 'settings': { 'branch': 'master',
                 'git_url': 'https://github.com/arun477/miniai',
                 'lib_path': 'miniai'},
   'syms': { 'miniai.core': {'miniai.core.foo': ('core.html#foo', 'miniai/core.py')},
-            'miniai.datasets': {'miniai.datasets.boo': ('datasets.html#boo', 'miniai/datasets.py')}}}
+            'miniai.datasets': {},
+            'miniai.training': { 'miniai.training.Dataset': ('training.html#dataset', 'miniai/training.py'),
+                                 'miniai.training.Dataset.__getitem__': ('training.html#dataset.__getitem__', 'miniai/training.py'),
+                                 'miniai.training.Dataset.__init__': ('training.html#dataset.__init__', 'miniai/training.py'),
+                                 'miniai.training.Dataset.__len__': ('training.html#dataset.__len__', 'miniai/training.py'),
+                                 'miniai.training.accuracy': ('training.html#accuracy', 'miniai/training.py'),
+                                 'miniai.training.fit': ('training.html#fit', 'miniai/training.py'),
+                                 'miniai.training.get_dls': ('training.html#get_dls', 'miniai/training.py'),
+                                 'miniai.training.report': ('training.html#report', 'miniai/training.py')}}}
